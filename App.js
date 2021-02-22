7c5d8d267ae712b92c9fb052160bcde9aea6e8d4
Greifelt228 <68552897+Greifelt228@users.noreply.github.com>
2021-02-22 19:35:44 +0200

    apiStart

--- App.js
+++ App.js
@@ -1,22 +1,51 @@
+import axios from 'axios'
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data,setData]= useState();
+  const axios = require('axios');
+  // axios.get('https://pokeapi.co/api/v2/pokemon/')
+  // .then(function (response){
+  //   setData(response.data);
+  //   console.log(data.results);
+  // })
+  async function getPokemon(){
+    try{
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+      setData(response.data);
+      console.log(data.results);
+    }catch (error){
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getPokemon();
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your xyz!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      {/* {data.map((el)=>{
+        return( 
+          <View>
+            <Text>
+                {el.name}
+            </Text>
+            <Text>
+              {el.url}
+            </Text>
+          </View>)
+      })} */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginLeft: 30,
+    paddingTop: 60,
+    marginRight:30,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
